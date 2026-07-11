aacaf44815ad8812eb0de42aaa59f080cefc0c3d
Basel osama ibrahim Ahmed <baselosama5005@gmail.com>
2026-07-11 23:28:13 +0300

    30062026 change

--- app/Http/Controllers/Dashboard/CompanyController.php
+++ app/Http/Controllers/Dashboard/CompanyController.php
@@ -77,10 +77,8 @@ class CompanyController extends Controller
     {
         $query = Company::with(['owner', 'employees', 'branches']);
 
-        $company = $id !== null
-            ? $query->find($id)
-            : $query->where('user_id', auth()->id())->first();
-
+        $company = $id !== null ? $query->find($id) : $query->where('user_id', auth()->id())->first();
+        dd(auth()->id(),$id,$company);
         if (! $company) {
             return ResponseHelper::error(
                 __('messages.company_not_found'),
